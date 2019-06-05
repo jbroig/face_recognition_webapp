28fcc94439784c156a42f6805c06465493112d8a
jbroig <juanbravoroig@gmail.com>
2019-06-05 18:40:09 +0200

    first commit

---
+++ camera.py
@@ -0,0 +1,93 @@
+from flask import Flask, render_template, Response
+# from camera import VideoCamera
+
+import cv2
+import imutils
+import time
+import face_recognition
+import numpy as np
+import pickle
+
+data = pickle.loads(open("encodings.pickle", "rb").read())
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+class VideoCamera(object):
+    def __init__(self):
+        # Using OpenCV to capture from device 0. If you have trouble capturing
+        # from a webcam, comment the line below out and use a video file
+        # instead.
+        self.video = cv2.VideoCapture(0)
+        # If you decide to use video.mp4, you must have this file in the folder
+        # as the main.py.
+        # self.video = cv2.VideoCapture('video.mp4')
+    
+    def __del__(self):
+        self.video.release()
+    
+    def get_frame(self):
+        success, frame = self.video.read()
+        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
+        # so we must encode it into JPEG in order to correctly display the
+        # video stream.
+
+        # convert the input frame from BGR to RGB then resize it to have
+        # a width of 750px (to speedup processing)
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        rgb = imutils.resize(frame, width=750)
+        r = frame.shape[1] / float(rgb.shape[1])
+
+        # detect the (x, y)-coordinates of the bounding boxes
+        # corresponding to each face in the input frame, then compute
+        # the facial embeddings for each face
+        boxes = face_recognition.face_locations(rgb, model="hog")
+        encodings = face_recognition.face_encodings(rgb, boxes)
+        names = []
+
+        # loop over the facial embeddings
+        for encoding in encodings:
+            # attempt to match each face in the input image to our known
+            # encodings
+            matches = face_recognition.compare_faces(data["encodings"], encoding, tolerance=0.5)
+            name = "Unknown"
+
+            # check to see if we have found a match
+            if True in matches:
+                # find the indexes of all matched faces then initialize a
+                # dictionary to count the total number of times each face
+                # was matched
+                matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+                counts = {}
+
+                # loop over the matched indexes and maintain a count for
+                # each recognized face face
+                for i in matchedIdxs:
+                    name = data["names"][i]
+                    counts[name] = counts.get(name, 0) + 1
+
+                # determine the recognized face with the largest number
+                # of votes (note: in the event of an unlikely tie Python
+                # will select first entry in the dictionary)
+                name = max(counts, key=counts.get)
+
+            names.append(name)
+            # print(name)
+
+        # loop over the recognized faces
+        for ((top, right, bottom, left), name) in zip(boxes, names):
+            # rescale the face coordinates
+            top = int(top * r)
+            right = int(right * r)
+            bottom = int(bottom * r)
+            left = int(left * r)
+
+            # draw the predicted face name on the image
+            cv2.rectangle(frame, (left, top), (right, bottom),
+                (0, 255, 0), 2)
+            y = top - 15 if top - 15 > 15 else top + 15
+            # cv2.putText(img, text, position, font, fontScale, color, thickness, lineType, bottomLeftOrigin)
+            cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
+
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        return jpeg.tobytes()
+
+        
