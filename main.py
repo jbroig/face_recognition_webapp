f13470354ebe0ebbdfa9ef61988758229acdd016
jbroig <juanbravoroig@gmail.com>
2019-06-06 12:09:07 +0200

    updated code

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+
     return render_template('index.html')
 
 def gen(camera):
@@ -32,4 +33,4 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=False)
+    app.run(host='0.0.0.0', debug=True)
